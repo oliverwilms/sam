861bf3264fb01698ceee96ab42d3fd7e08d95311
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2021-08-21 18:53:43 -0500

    Update sam.sh

--- sam.sh
+++ sam.sh
@@ -9,6 +9,6 @@ sed "s#eman.com#$host#g" openssl.template > openssl.conf
 # openssl req -config openssl.conf -new -nodes -x509 -newkey rsa:2048 -sha256 -keyout apache-selfsigned.key -out apache-selfsigned.crt -days 3650
 # sleep 5
 service apache2 restart
-service ssh start
+# service ssh start
 # exit 0
 exit $?
